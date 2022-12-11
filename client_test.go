390daa7139b80ff9c3c129d2f9bd415b1a7fab27
Hiromu OCHIAI <otiai10@gmail.com>
2022-12-11 18:51:43 +0900

    Add more tests (client)

--- client_test.go
+++ client_test.go
@@ -1,6 +1,7 @@
 package openaigo
 
 import (
+	"context"
 	"testing"
 
 	. "github.com/otiai10/mint"
@@ -8,4 +9,15 @@ import (
 
 func TestNewClient(t *testing.T) {
 	Expect(t, NewClient("xxx")).TypeOf("*openaigo.Client")
+}
+
+func TestClient_internal(t *testing.T) {
+	client := NewClient("invalid")
+	client.Organization = "org-xxx"
+	_, err := client.ListModels(context.TODO())
+	Expect(t, err).Not().ToBe(nil)
+
+	client.BaseURL = mockserver.URL
+	_, err = client.RetrieveModel(nil, "notfound")
+	Expect(t, err).Not().ToBe(nil)
 }
