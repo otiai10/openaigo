f9d6f9af8137e482b637af19a091ad470e983fa4
Hiromu OCHIAI <otiai10@gmail.com>
2024-05-14 08:39:55 +0900

    Add GPT-4o

--- models.go
+++ models.go
@@ -37,12 +37,14 @@ type ModelRetrieveResponse ModelData
 const (
 
 	// {{{ https://platform.openai.com/docs/models/gpt-4
-	GPT4          = "gpt-4"
-	GPT4_0314     = "gpt-4-0314"
-	GPT4_0613     = "gpt-4-0613"
-	GPT4_32K      = "gpt-4-32k"
-	GPT4_32K_0314 = "gpt-4-32k-0314"
-	GPT4_32K_0613 = "gpt-4-32k-0613"
+	GPT4o          = "gpt-4o"
+	GPT4o_20240513 = "gpt-4o-2024-05-13"
+	GPT4           = "gpt-4"
+	GPT4_0314      = "gpt-4-0314"
+	GPT4_0613      = "gpt-4-0613"
+	GPT4_32K       = "gpt-4-32k"
+	GPT4_32K_0314  = "gpt-4-32k-0314"
+	GPT4_32K_0613  = "gpt-4-32k-0613"
 	// }}}
 
 	// {{{ https://platform.openai.com/docs/models/gpt-3-5
